b7bdef386461916f3b3e1df1ec5f2b302ff2d185
MauricioAires <mauricioaires015@gmail.com>
2022-11-15 22:58:46 -0300

    feat: criar continentInfo component

---
+++ src/components/ContinentInfo.tsx
@@ -0,0 +1,46 @@
+import { Flex, Text } from '@chakra-ui/react'
+import { Container } from './Container'
+
+export function ContinentInfo() {
+  return (
+    <Container>
+      <Flex p="5">
+        <Text flex="1" fontSize="2xl" color="dark.800" fontWeight="normal">
+          A Europa é, por convenção, um dos seis continentes do mundo.
+          Compreendendo a península ocidental da Eurásia, a Europa geralmente
+          divide-se da Ásia a leste pela divisória de águas dos montes Urais, o
+          rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste
+        </Text>
+
+        <Flex flex="1" justify="center" gap="42" align="center">
+          <Flex direction="column" align="center" justify="center">
+            <Text color="yellow.500" fontSize="5xl" fontWeight="semibold">
+              50
+            </Text>
+            <Text color="dark.800" fontSize="2xl" fontWeight="semibold">
+              países
+            </Text>
+          </Flex>
+
+          <Flex direction="column" align="center" justify="center">
+            <Text color="yellow.500" fontSize="5xl" fontWeight="semibold">
+              60
+            </Text>
+            <Text color="dark.800" fontSize="2xl" fontWeight="semibold">
+              línguas
+            </Text>
+          </Flex>
+
+          <Flex direction="column" align="center" justify="center">
+            <Text color="yellow.500" fontSize="5xl" fontWeight="semibold">
+              27
+            </Text>
+            <Text color="dark.800" fontSize="2xl" fontWeight="semibold">
+              cidades +100
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Container>
+  )
+}
