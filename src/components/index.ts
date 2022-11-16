714b524a1cb28961e4bfd93b6974cf91df6f2667
MauricioAires <mauricioaires015@gmail.com>
2022-11-15 23:00:06 -0300

    feat: exportar componentes de um unico lugar

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { Container } from './Container'
+export { ContinentsSlide } from './ContinentsSlide'
+export { TravelTypes } from './TravelTypes'
+export { Header } from './Header'
+export { Banner } from './Banner'
+export { Divider } from './Divider'
+export { ContinentInfo } from './ContinentInfo'
